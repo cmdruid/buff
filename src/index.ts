093a5a47a3212c3fe6f05ca3468ef86cac37a1c2
cmd <cmd@yggdrasil.onion>
2022-11-06 16:14:21 -0600

    update

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import Buff   from './buff.js'
 import Stream from './stream.js'
 import Type   from './type.js'
-// import BaseX  from './basex.js'
-// import Bech32 from './bech32.js'
+import BaseX  from './basex.js'
+import Bech32 from './bech32.js'
 
 export {
   Buff,
   Stream,
   Type,
-  // BaseX,
-  // Bech32
+  BaseX,
+  Bech32
 }
