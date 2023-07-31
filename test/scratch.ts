29f5f9e724f9f0bc5a1ade4ce2e2ecf674959141
cmd <cmd@yggdrasil.onion>
2023-07-31 12:50:55 -0500

    update

--- test/scratch.ts
+++ test/scratch.ts
@@ -1,8 +1,7 @@
-import { Buff } from '../src/index.js'
+import { bech32 } from '@scure/base'
 
-const test_str = 'https://walletofsatoshi.com/.well-known/lnurlp/jeeringmarch53'
+const test_enc  = 'lnbc1u1pjvy84epp5zrapr3w7tqelvjzwm0rwsac2ga79m982uruducydr2u6zwlhpasqhp5fe47lwjexge0lff7ru2g6757g35qajscy39hsz4dvqe97gnt3d3scqzzsxqyz5vqsp5ptv9dz544r5pxd3gkulqelakrtmx4nf47xw4mmm8a0u8j2up7mqs9qyyssqumxjespzkuwzdppw3hzkawgdedjyu2e0wnsk3t3y8g7mkpz49nn9rlrzsj07tz3hjnld80j749069puz9uanhr55p9ngw46cy2w295qpktsz9y'
+const { bytes } = bech32.decodeToBytes(test_enc)
+const decoded   = new TextDecoder().decode(bytes)
 
-const encoded = Buff.str(test_str).toBech32('lnurl')
-
-console.log(encoded)
-
+console.log('decoded:', decoded)
